50f276df6bdd87e7257413ef71adedca59fd3c65
ShirleyLe3 <synchrotronflux@yahoo.com>
2020-09-11 21:48:56 -0400

    Sprint

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import React, { useState } from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// import Login from "./components/Login";
+// import "./styles.scss";
+
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import styled from "styled-components";
 
 import Login from "./components/Login";
-import "./styles.scss";
+import PrivateRoute from "./components/PrivateRoute";
+import Friends from "./components/Friends";
+import addFriend from "./components/addFriend";
+
 
 function App() {
   return (
@@ -17,5 +27,31 @@ function App() {
     </Router>
   );
 }
+
+const Div = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Div>
+          <Link to="/login">Login</Link>
+          <Link to="/protected">Friends List</Link>
+          <Link to="/addFriend">Add a Friend</Link>
+        </Div>
+        <Switch>
+          <PrivateRoute exact path="/protected" component={Friends} />
+          <Route path="/login" component={Login} />
+          <PrivateRoute path="/addFriend" component={addFriend} />
+          {/* <PrivateRoute path="/dashboard" component={Dashboard} /> */}
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
 
 export default App;
--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -5,3 +5,11 @@ import BubblePage from "./BubblePage";
 test("Fetches data and renders the bubbles", () => {
   // Finish this test
 });
+
+
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
